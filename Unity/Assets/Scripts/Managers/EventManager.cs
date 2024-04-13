e19eeb63b44efd861ac5a1c4036abf342d600ea4
Trevor <reevest2@gmail.com>
2024-04-12 21:59:28 -0400

    add controllers and managers. Player controller script

---
+++ Unity/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager
+{
+    public static event UnityAction OnPlayerMoved;
+    public static void PlayerMoved() => OnPlayerMoved?.Invoke();
+}
